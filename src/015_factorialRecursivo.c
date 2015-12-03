59007202e761751b2a444ca1e2408fefe41dbe7e
cafernandezlo <cafernandezlo@gmail.com>
2015-12-03 10:22:05 +0100

    015_factorialRecursivo.c

---
+++ src/015_factorialRecursivo.c
@@ -0,0 +1,21 @@
+/**Programa: FactorialRecursivo
+ * Fuente: Libro asignatura Fundamentos de Programación UNED. Cerrada Somolinos et al.
+ * Descripción: El objetivo buscado es introducir al alumno en el uso
+ * de la recursividad */
+
+#include <stdio.h>
+
+int FactorialRecursivo(int n){
+	if(n<=1){
+		return 1;
+	} else{
+		return n*FactorialRecursivo(n-1);
+	}
+}
+
+int main(){
+	for (int i=0,i<=10;i++){
+		printf("%2d! vale:%10d\n",i,FactorialRecursivo(i));
+	}
+	return 0;
+}
